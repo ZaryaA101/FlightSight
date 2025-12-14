0de800eba1734727bb758fe16500e1fcc3cd0e00
Zarya Amin <zarya.amin01@student.csulb.edu>
2025-12-13 22:39:31 -0800

    Restructure frontend folders + add Booking page + updates

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,15 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const searchBtn = document.querySelector(".search-btn");
-    if (searchBtn) {
-    searchBtn.addEventListener("click", function() { 
-        window.location.href = "flightRoute.html";
+  const searchBtn = document.querySelector(".search-btn");
+  if (searchBtn) {
+    searchBtn.addEventListener("click", function() {
+      window.location.href = "flightRoute.html";
     });
-    }
+  }
 
-    const continueBtn = document.querySelector(".continue-btn");
-    if (continueBtn) {
+  const continueBtn = document.querySelector(".continue-btn");
+  if (continueBtn) {
     continueBtn.addEventListener("click", function() {
-        window.location.href = "calendar.html";
+      window.location.href = "calendar.html";
     });
-    }
+  }
+
+  const selectDatesBtn = document.getElementById("selectDatesBtn");
+  if (selectDatesBtn) {
+    selectDatesBtn.addEventListener("click", function () {
+      window.location.href = "Booking.html";
+    });
+  }
 });
